defaf8ef39d849644e29ee79ee5fc22aa948d696
jtpro123 <jt.prorleans@gmail.com>
2023-03-02 18:37:47 +0800

    update

---
+++ app/Models/_blogmodel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class _blogmodel
+{
+    public static function all(){
+        $blogs = [
+            [
+                'id'  => '1',
+                'blog_title' => 'Easily Grow your Business Earn More Money',
+                'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+
+            ],
+            [
+                'id'  => '2',
+                'blog_title' => 'Easily',
+                'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+                
+            ],
+            [
+                'id'  => '3',
+                'blog_title' => 'hardly',
+                'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+                
+            ]
+
+            ];
+            return ($blogs);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         //\App\Models\User::factory(10)->create();
+         DB::table('blogmodels')->insert(
+            [
+                'id'  => '1',
+                'blog_title' => 'Easily Grow your Business Earn More Money',
+                'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+            ]
+            );
+            DB::table('blogmodels')->insert(
+                [
+                    
+                'id'  => '2',
+                'blog_title' => 'Easily',
+                'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+                ]
+                );
+            DB::table('blogmodels')->insert(
+                [
+                    'id'  => '3',
+                    'blog_title' => 'hardly',
+                    'blog_desc' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
+                    injected humour, or randomised words There uffered alteration in some form, by injected humour, or randomised words',
+                ]
+                );
     }
 }
